baa4b619a2efec4db6274f253b4a2e683f58b312
MrSolomans <77716773+MrSolomans@users.noreply.github.com>
2023-07-05 15:53:00 +0800

    Update test.py

--- hackpy/test.py
+++ hackpy/test.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-"""
-使用requests请求隧道服务器
-请求http和https网页均适用
-"""
-import os
-import random
-with open(os.getcwd()+'/user-agents.txt') as f:
-                agent = f.readlines()
-                print(type(random.choice(agent)))
